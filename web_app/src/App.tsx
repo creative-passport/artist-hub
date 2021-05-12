e51fe03bffccc0f0f26f0b122f61d183369a0796
Richard Strand <richard@strand.me.uk>
2021-05-12 18:34:35 +0100

    Add a basic footer

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import { Home } from './pages/home';
 import { AuthProvider } from './providers/AuthProvider';
 import { Layout } from './components/Layout';
@@ -14,49 +14,68 @@ import DialogProvider from './providers/DialogProvider';
 import { NotFound } from './pages/NotFound';
 import { Header } from './components/Header';
 import { appTheme } from './theme';
+import { Footer } from './components/Footer';
 
 const queryClient = new QueryClient();
 
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  main: {
+    flex: '1 0 auto',
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
       <Router>
         <QueryClientProvider client={queryClient}>
           <DialogProvider>
-            <Header />
-            <Switch>
-              <Route path="/p/:username">
-                <ArtistPage />
-              </Route>
-              <Route>
-                <AuthProvider>
-                  <Layout>
-                    <Switch>
-                      <PrivateRoute
-                        path="/"
-                        exact
-                        renderPublic={() => <PublicHome />}
-                      >
-                        <Home />
-                      </PrivateRoute>
-                      <PrivateRoute path="/admin/artistpages/:artistId">
-                        <ArtistPageShow />
-                      </PrivateRoute>
-                      <PrivateRoute path="/admin/artistpages">
-                        <ArtistPageIndex />
-                      </PrivateRoute>
-                      <Route path="/p/:username">
-                        <ArtistPage />
-                      </Route>
-                      <Route>
-                        <NotFound />
-                      </Route>
-                    </Switch>
-                  </Layout>
-                </AuthProvider>
-              </Route>
-            </Switch>
+            <div className={classes.container}>
+              <div className={classes.main}>
+                <Header />
+                <Switch>
+                  <Route path="/p/:username">
+                    <ArtistPage />
+                  </Route>
+                  <Route>
+                    <AuthProvider>
+                      <Layout>
+                        <Switch>
+                          <PrivateRoute
+                            path="/"
+                            exact
+                            renderPublic={() => <PublicHome />}
+                          >
+                            <Home />
+                          </PrivateRoute>
+                          <PrivateRoute path="/admin/artistpages/:artistId">
+                            <ArtistPageShow />
+                          </PrivateRoute>
+                          <PrivateRoute path="/admin/artistpages">
+                            <ArtistPageIndex />
+                          </PrivateRoute>
+                          <Route path="/p/:username">
+                            <ArtistPage />
+                          </Route>
+                          <Route>
+                            <NotFound />
+                          </Route>
+                        </Switch>
+                      </Layout>
+                    </AuthProvider>
+                  </Route>
+                </Switch>
+              </div>
+              <Footer />
+            </div>
           </DialogProvider>
         </QueryClientProvider>
       </Router>
---
+++ web_app/src/components/Footer.tsx
@@ -0,0 +1,87 @@
+import { Container, Grid, Link, makeStyles } from '@material-ui/core';
+import { PropsWithChildren } from 'react';
+import { ReactComponent as LogoWhite } from '../images/logo-white.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+    flexShrink: 0,
+    '& a': {
+      textDecoration: 'underline',
+    },
+  },
+  name: {
+    fontSize: 18,
+    textTransform: 'uppercase',
+  },
+  footer: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    borderRadius: '32px 32px 0px 0px',
+    background: '#222222',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logoAbout: {
+    display: 'flex',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  logo: {
+    marginRight: theme.spacing(2),
+  },
+  links: {
+    flexGrow: 1,
+  },
+}));
+
+interface FooterLinkProps {
+  href: string;
+}
+function FooterLink({ href, children }: PropsWithChildren<FooterLinkProps>) {
+  return (
+    <Link href={href} color="inherit">
+      {children}
+    </Link>
+  );
+}
+
+export function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="md">
+        <footer className={classes.footer}>
+          <div className={classes.logoAbout}>
+            <LogoWhite className={classes.logo} />
+            <div>
+              <FooterLink href="https://www.creativepassport.net/artist-hub/">
+                About
+                <br />
+                <span className={classes.name}>Artist Hub</span>
+              </FooterLink>
+            </div>
+          </div>
+          <div className={classes.links}>
+            <Grid container>
+              <Grid item xs>
+                <FooterLink href="https://www.creativepassport.net">
+                  The Creative Passport
+                </FooterLink>
+              </Grid>
+
+              <Grid item xs>
+                <FooterLink href="https://github.com/creative-passport/artist-hub">
+                  Source code
+                </FooterLink>
+              </Grid>
+            </Grid>
+          </div>
+        </footer>
+      </Container>
+    </div>
+  );
+}
